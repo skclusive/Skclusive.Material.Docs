ad329b25259afaab4a9e069702d2b12d326786f9
skclusive <skclusive@outlook.com>
2020-03-27 08:00:02 +0530

    Home page updates

--- Docs.Server.Host/Program.cs
+++ Docs.Server.Host/Program.cs
@@ -22,6 +22,7 @@ namespace Skclusive.Material.Docs.Server.Host
             Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseStaticWebAssets();
                     webBuilder.UseStartup<Startup>();
                 });
     }
